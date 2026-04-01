b47584c913f622508acfd5c76ef46c483e1648f5
ghparenty <ghparenty@gmail.com>
2026-04-01 17:06:05 +0200

    debut python

--- tp1/python/src/etudiant.py
+++ tp1/python/src/etudiant.py
@@ -1,5 +1,27 @@
 from personne import Personne
+import numpy as np
 
 class Etudiant(Personne):
-    def __init__(self):
-        pass
+    def __init__(self, nom, age, numero):
+        super().__init__(nom, age)
+        self.__numero = numero
+        self.__matieres = {}
+        self.__moyenne = 0
+
+    @property
+    def numero(self):
+        return self.__numero
+    
+    def ajouter_matiere(self, matiere):
+        if 
+    
+    def ajouter_note(self, matiere, note):
+        if type(note) == float or type(note) == int:
+            self.__notes.append(note)
+        else:
+            raise TypeError("la note n'est pas au bon format")
+        
+    @property
+    def moyenne(self):
+        self.__moyenne = np.mean(self.notes)
+        return self.__moyenne
